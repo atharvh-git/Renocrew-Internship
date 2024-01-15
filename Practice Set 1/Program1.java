cb025c0e9679592402ef098481a957d9bd325c11
atharvhatwalne <156585805+atharvhatwalne@users.noreply.github.com>
2024-01-15 12:19:11 +0530

    Program 1

---
+++ Practice Set 1/Program1.java
@@ -0,0 +1,12 @@
+// Write a program to sum three numbers.
+import java.util.*;
+public class Program1 {
+    public static void main(String[] args) {
+        //taking three numbers
+        int a = 12, b = 10, c = 20;
+        //adding all the three numbers and storing the result in a variable called 'sum'
+        int sum = a + b + c; 
+        System.out.println(sum); //printing the sum variable
+    }
+}
+
