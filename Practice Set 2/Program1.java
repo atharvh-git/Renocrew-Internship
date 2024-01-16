758aeea52439ed52f0e584c710699b0a11ef3739
atharvhatwalne <156585805+atharvhatwalne@users.noreply.github.com>
2024-01-16 10:34:38 +0530

    Practice Set 2

---
+++ Practice Set 2/Program1.java
@@ -0,0 +1,9 @@
+// What will be the result of the following expression?
+// float num = 7/4 * 9/2;
+
+public class Program1 {
+    public static void main(String[] args) {
+        float num = 7 / 4.0f * 9 / 2.0f;
+        System.out.println(num);
+    }
+}
