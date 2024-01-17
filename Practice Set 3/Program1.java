59e1b8592ea6b5f99a4f46742eb6fbde15ff5d2c
atharvhatwalne <156585805+atharvhatwalne@users.noreply.github.com>
2024-01-17 11:38:52 +0530

    Practice Set 3

---
+++ Practice Set 3/Program1.java
@@ -0,0 +1,11 @@
+// Write a java program to convert a string to lowercase.
+
+public class Program1 {
+    public static void main(String[] args) {
+        String upper_str = "HELLO WORLD";
+        // To convert the above string to lowercase, we have
+        // a method, toLowercase()
+        String lower_str = upper_str.toLowerCase();
+        System.err.println(lower_str); // printing the result
+    }
+}
