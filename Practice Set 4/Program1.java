958a9756c46b87b71afa808690d17f6581c77f90
atharvhatwalne <156585805+atharvhatwalne@users.noreply.github.com>
2024-01-18 14:23:16 +0530

    Practice Set 4

---
+++ Practice Set 4/Program1.java
@@ -0,0 +1,17 @@
+// Find the output of following code snippet:
+// int a = 10;
+// if(a = 11)
+//   System.out.println("I'm 11");
+// else
+//   System.out.println("I'm not 11");
+
+public class Program1 {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a == 11) {
+            System.out.println("I am 11");
+        } else {
+            System.out.println("I am not 11");
+        }
+    }
+}
