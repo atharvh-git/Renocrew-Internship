123345c140e6e9f974d747dcf626b4583e6a03e8
Atharv Hatwalne <126438037+atharvh-git@users.noreply.github.com>
2024-02-01 12:51:44 +0530

    Practice Set 11

---
+++ Practice Set 11/Abstraction1.java
@@ -0,0 +1,13 @@
+// Create an abstract class Pen with methods Write() and reFill() as abstract methods. 
+
+abstract class Pen {
+    abstract void Write();
+
+    abstract void reFill();
+}
+
+public class Abstraction1 {
+    public static void main(String[] args) {
+        System.out.println("Created Write() and reFill() as abstract methods successfully!");
+    }
+}
