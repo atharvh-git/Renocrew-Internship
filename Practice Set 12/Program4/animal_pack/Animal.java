6637b16191f51223fdc70c252c94b003aaf7af51
Atharv Hatwalne <126438037+atharvh-git@users.noreply.github.com>
2024-02-02 13:27:37 +0530

    Practice Set 12

---
+++ Practice Set 12/Program4/animal_pack/Animal.java
@@ -0,0 +1,9 @@
+package animal_pack;
+
+public class Animal {
+    // Default (package-private) property
+    String defaultProperty = "I am a default property";
+
+    // Protected property
+    protected String protectedProperty = "I am a protected property";
+}
