9d8dc7b9ab68234858bec088e4d68c7e84bf4dc0
Atharv Hatwalne <126438037+atharvh-git@users.noreply.github.com>
2024-02-02 14:36:53 +0530

    Practice Set 12

---
+++ Practice Set 12/com/cwh/shape/Shape.java
@@ -0,0 +1,18 @@
+// Task: You have to create a package named com.codewithharry.shape. 
+// This package should have individual classes forRectangle, Square, Circle, Cylinder and Sphere. 
+// These classes should use inheritance to properly manage the code and also include methods to 
+// calculate volume, surface area and use getters/setters for dimensions.
+
+package com.cwh.shape;
+
+public class Shape {
+
+    // Default methods for volume and surface area
+    public double calcVolume() {
+        return 0.0;
+    }
+
+    public double calcSurfaceArea() {
+        return 0.0;
+    }
+}
