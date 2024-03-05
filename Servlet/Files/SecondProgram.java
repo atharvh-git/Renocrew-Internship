d656edd1adbf6e30e9067cf46da820d4e620e60b
Atharv Hatwalne <126438037+atharvh-git@users.noreply.github.com>
2024-03-05 14:13:13 +0530

    Servlet Programming

---
+++ Servlet/Files/SecondProgram.java
@@ -0,0 +1,25 @@
+// Creating servlet using GenericServlet class
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class SecondProgram extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Generic Servlet");
+		
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		out.println("DATE " + new Date().toString());
+	}
+	
+}
