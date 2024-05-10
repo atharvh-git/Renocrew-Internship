202bd1dadc29cc273e4a4d83676bf93b21b94eb9
Atharv Hatwalne <126438037+atharvh-git@users.noreply.github.com>
2024-05-10 11:40:17 +0530

    Create UserRepo.java

---
+++ Project Folder/Java Source Code files/UserRepo.java
@@ -0,0 +1,12 @@
+package com.sheryians.major.repo;
+
+import com.sheryians.major.model.User;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+   Optional<User> findUserByEmail(String email);
+}
